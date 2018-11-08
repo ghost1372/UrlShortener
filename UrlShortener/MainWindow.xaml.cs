b4ba9585e7c58f44467c02c25b78e0fe1d939505
Mahdi Hosseini <mahdidvb72@gmail.com>
2018-11-08 10:37:50 +0330

    add plink service

--- UrlShortener/MainWindow.xaml.cs
+++ UrlShortener/MainWindow.xaml.cs
@@ -26,10 +26,14 @@ namespace UrlShortener
     /// </summary>
     public partial class MainWindow : WindowBorderless
     {
+        #region API Key
         private const string BitlyApiKey = "R_c597e397b606436fa6a9179626da61bb";
         private const string BitlyLoginKey = "o_1i6m8a9v55";
         private const string OpizoApiKey = "3DD3A7CD39B37BC8CBD9EFEEAC0B03DA";
+        private const string PlinkApiKey = "RScobms6dUhn";
 
+        #endregion
+        #region Update Variable
         private string newVersion = string.Empty;
 
         private string ChangeLog = string.Empty;
@@ -41,19 +45,26 @@ namespace UrlShortener
         public const string UpdateUrlTag = "url"; //Defined in Xml file
         public const string UpdateChangeLogTag = "changelog";
         public static string getAppVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        #endregion
+
+        private List<ShorterList> shorterList = new List<ShorterList>();
 
         public MainWindow()
         {
             InitializeComponent();
             Growl.SetGrowlPanel(PanelMessage);
+
+            //get default values
             cmbService.SelectedIndex = Properties.Settings.Default.Setting;
             cmbListService.SelectedIndex = Properties.Settings.Default.Setting;
-
             var topMost = Properties.Settings.Default.TopMost;
             tgTop.IsChecked = topMost;
             this.Topmost = topMost;
         }
 
+        #region Shorten Services
+
+        //get Method
         public string AtrabIr(string longUrl)
         {
             string link = string.Empty;
@@ -101,6 +112,20 @@ namespace UrlShortener
             }
         }
 
+        public string PlinkShorten(string longUrl, string customURL = "")
+        {
+            string link = string.Empty;
+            using (var wb = new WebClient())
+            {
+                var response = wb.DownloadString("https://plink.ir/api?api=" + PlinkApiKey + "& url="+ longUrl + "&custom=" + customURL);
+                var root = JsonConvert.DeserializeObject<PlinkData>(response);
+                if (root.error.Contains("0"))
+                    link = root.@short;
+                else
+                    Growl.Error(root.msg);
+            }
+            return link;
+        }
         public string OpizoShorten(string longUrl)
         {
             string link = string.Empty;
@@ -122,7 +147,6 @@ namespace UrlShortener
             }
             return link;
         }
-
         public string YonShorten(string longUrl, string customURL = "")
         {
             string link = string.Empty;
@@ -143,6 +167,15 @@ namespace UrlShortener
             return link;
         }
 
+        #endregion
+
+        #region Deserialize Class
+        public class PlinkData
+        {
+            public string error { get; set; }
+            public string @short { get; set; }
+            public string msg { get; set; }
+        }
         public class OpizoData
         {
             public string url { get; set; }
@@ -170,6 +203,14 @@ namespace UrlShortener
             public AtrabData data { get; set; }
         }
 
+        public class ShorterList
+        {
+            public string Link { get; set; }
+            public string ShortLink { get; set; }
+        }
+        #endregion
+
+        #region Url Shorten
         private void txtUrl_TextChanged(object sender, TextChangedEventArgs e)
         {
             if (string.IsNullOrEmpty(txtUrl.Text))
@@ -185,21 +226,6 @@ namespace UrlShortener
                 btn.IsEnabled = false;
 
             stck.Visibility = Visibility.Hidden;
-        }
-
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            var tag = sender as MenuItem;
-            switch (tag.Tag)
-            {
-                case "Update":
-                    CheckUpdate();
-                    break;
-
-                case "About":
-                    Growl.Info("Coded by Mahdi Hosseini\n Contact: mahdidvb72@gmail.com");
-                    break;
-            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -226,6 +252,10 @@ namespace UrlShortener
                     break;
 
                 case 4:
+                    if (string.IsNullOrEmpty(txtCustom.Text))
+                        txtUrl.Text = PlinkShorten(txtUrl.Text);
+                    else
+                        txtUrl.Text = PlinkShorten(txtUrl.Text, txtCustom.Text);
                     break;
             }
             Clipboard.SetText(txtUrl.Text);
@@ -237,15 +267,9 @@ namespace UrlShortener
             Properties.Settings.Default.Setting = cmbService.SelectedIndex;
             Properties.Settings.Default.Save();
         }
+        #endregion
 
-        public class ShorterList
-        {
-            public string Link { get; set; }
-            public string ShortLink { get; set; }
-        }
-
-        private List<ShorterList> shorterList = new List<ShorterList>();
-
+        #region Group Link Shorten
         private void btnLoad_Click(object sender, RoutedEventArgs e)
         {
             shorterList.Clear();
@@ -293,6 +317,9 @@ namespace UrlShortener
                             case 3:
                                 shorterList.Add(new ShorterList { ShortLink = AtrabIr(longLink) });
                                 break;
+                            case 4:
+                                shorterList.Add(new ShorterList { ShortLink = PlinkShorten(longLink) });
+                                break;
                         }
                     }
                 }
@@ -309,42 +336,9 @@ namespace UrlShortener
             }), DispatcherPriority.Background);
         }
 
-        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
-        {
-            if (tgTop.IsChecked == true)
-                Properties.Settings.Default.TopMost = true;
-            else
-                Properties.Settings.Default.TopMost = false;
+        #endregion
 
-            Properties.Settings.Default.Save();
-            this.Topmost = tgTop.IsChecked.Value;
-        }
-
-        private void WindowBorderless_StateChanged(object sender, EventArgs e)
-        {
-            if (WindowState == WindowState.Maximized)
-            {
-                WindowState = WindowState.Normal;
-            }
-        }
-
-        private void WindowBorderless_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            this.Width = 600;
-            if (tabc.SelectedIndex == 0)
-                this.Height = 220;
-            else
-                this.Height = 400;
-        }
-
-        private void tabc_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (tabc.SelectedIndex == 0)
-                this.Height = 220;
-            else
-                this.Height = 400;
-        }
-
+        #region Update
         private void CheckUpdate()
         {
             try
@@ -442,6 +436,49 @@ namespace UrlShortener
                 }
             }
             return (false);
+        }
+
+        #endregion
+
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (tgTop.IsChecked == true)
+                Properties.Settings.Default.TopMost = true;
+            else
+                Properties.Settings.Default.TopMost = false;
+
+            Properties.Settings.Default.Save();
+            this.Topmost = tgTop.IsChecked.Value;
+        }
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var tag = sender as MenuItem;
+            switch (tag.Tag)
+            {
+                case "Update":
+                    CheckUpdate();
+                    break;
+
+                case "About":
+                    Growl.Info("Coded by Mahdi Hosseini\n Contact: mahdidvb72@gmail.com");
+                    break;
+            }
+        }
+        private void WindowBorderless_StateChanged(object sender, EventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+            }
+        }
+
+        private void WindowBorderless_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            this.Width = 600;
+            if (tabc.SelectedIndex == 0)
+                this.Height = 220;
+            else
+                this.Height = 400;
         }
     }
 }
