2dfe2798f7301f4da2ac0a921387ead86b4e2b33
Mahdi Hosseini <mahdidvb72@gmail.com>
2019-10-10 16:11:04 +0330

    update libs

--- UrlShortener/App.xaml.cs
+++ UrlShortener/App.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using HandyControl.Data;
 using HandyControl.Tools;
-using Microsoft.Win32;
 using System;
 using System.Windows;
 
@@ -20,33 +19,21 @@ namespace UrlShortener
                 UpdateSkin(GlobalData.Config.Skin);
             }
 
-            RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion");
-
-            //Todo: Blur Effect not work in 1903 build
-            if (Convert.ToInt32(key.GetValue("ReleaseId")) < 1903)
-            {
-                ConfigHelper.Instance.SetSystemVersionInfo(CommonHelper.GetSystemVersionInfo());
-            }
+            ConfigHelper.Instance.SetSystemVersionInfo(CommonHelper.GetSystemVersionInfo());
 
             base.OnStartup(e);
         }
 
         internal void UpdateSkin(SkinType skin)
         {
-            ResourceDictionary skins0 = Resources.MergedDictionaries[0];
-            skins0.MergedDictionaries.Clear();
-            skins0.MergedDictionaries.Add(ResourceHelper.GetSkin(skin));
-            skins0.MergedDictionaries.Add(ResourceHelper.GetSkin(typeof(App).Assembly, "Resources/Themes", skin));
-
-            ResourceDictionary skins1 = Resources.MergedDictionaries[1];
-            skins1.MergedDictionaries.Clear();
-            skins1.MergedDictionaries.Add(new ResourceDictionary
+            Resources.MergedDictionaries.Clear();
+            Resources.MergedDictionaries.Add(new ResourceDictionary
+            {
+                Source = new Uri($"pack://application:,,,/HandyControl;component/Themes/Skin{skin.ToString()}.xaml")
+            });
+            Resources.MergedDictionaries.Add(new ResourceDictionary
             {
                 Source = new Uri("pack://application:,,,/HandyControl;component/Themes/Theme.xaml")
-            });
-            skins1.MergedDictionaries.Add(new ResourceDictionary
-            {
-                Source = new Uri("pack://application:,,,/UrlShortener;component/Resources/Themes/Theme.xaml")
             });
             Current.MainWindow?.OnApplyTemplate();
         }
